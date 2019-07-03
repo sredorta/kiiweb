82dcb82c9388e23fd3e070ebe5b4d03a13d1b465
sredorta <sergi.redorta@hotmail.com>
2019-07-03 15:30:32 +0200

    Moving environment to private

--- src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-  apiURL: "https://localhost:3000/api",
-  languages: ['fr','en','es'],
-  matInputAppearance: "fill",   //Mat input appearance "outline", "default", "fill"...
-  vapidPublic: 'BJ2ZA-q_PZ8yCexelSbjYCtCwZw0fmYVaEQ5MLMqhR4_1DVPDHcWOC1nL0LMKavdoV9bBrCmRDUQu_z35Uk8Evs',
-  gmapsKey:'AIzaSyDJX3_xSyhfZCyA2Z20f1d74X8sHOkX9dE',
-
-};
