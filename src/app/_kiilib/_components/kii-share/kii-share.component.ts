ebe19eb0b71a461426f7c5aa54c8f26c24b5c94b
sredorta <sergi.redorta@hotmail.com>
2019-09-24 16:55:31 +0200

    Updated share buttons for sending stats

--- src/app/_kiilib/_components/kii-share/kii-share.component.ts
+++ src/app/_kiilib/_components/kii-share/kii-share.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
 import { KiiMiscService } from '../../_services/kii-misc.service';
 import { KiiBaseAbstract } from '../../_abstracts/kii-base.abstract';
 import { isPlatformBrowser } from '@angular/common';
+import { Router } from '@angular/router';
+import { KiiApiStatsService } from '../../_services/kii-api-stats.service';
+import { StatAction } from '../../_models/stat';
 
 @Component({
   selector: 'kii-share',
@@ -12,18 +15,31 @@ export class KiiShareComponent extends KiiBaseAbstract implements OnInit {
 
   /**Shows or hides sharing depening on cookies */
   cookies : boolean = false;
+  /**Current url */
+  url : string = "";
 
-  constructor(private kiiMisc : KiiMiscService, @Inject(PLATFORM_ID) private platformId: any ) { super() }
+  constructor(private kiiStats: KiiApiStatsService,
+              private router: Router, 
+              private kiiMisc : KiiMiscService, 
+              @Inject(PLATFORM_ID) private platformId: any ) { super() }
 
   ngOnInit() {
     if (isPlatformBrowser(this.platformId)) {
+      this.url = window.location.href;
+      console.log("URL IS:",this.url)
       this.addSubscriber(
         this.kiiMisc.onCookiesChange().subscribe(res => {
           this.cookies = res;
         })
       )
     }
-
   }
+
+  sendStats(social:string) {
+      console.log("SENDING STATS !!");
+      this.kiiStats.send(StatAction.SOCIAL_CLICK, social);
+  }
+
+
 
 }
