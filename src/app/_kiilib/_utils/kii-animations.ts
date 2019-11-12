89ea455f802b187ee82b790ab3b05c55f039d49d
sredorta <sergi.redorta@hotmail.com>
2019-11-12 17:43:38 +0100

    Working on clients background

--- src/app/_kiilib/_utils/kii-animations.ts
+++ src/app/_kiilib/_utils/kii-animations.ts
@@ -7,8 +7,8 @@ export class KiiAinimations {
     public static contact() {
         return [
             trigger('contactAnimation', [
-                state('collapsed', style({transform: 'scale(0.6)'})),
-                state('expanded', style({transform: 'scale(1)'})),
+                state('collapsed', style({opacity:0.1})),
+                state('expanded', style({opacity:1})),
                 transition('expanded <=> collapsed', animate('.5s .3s cubic-bezier(0.4, 0.0, 0.2, 1)')),
               ]),
         ]
@@ -27,14 +27,14 @@ export class KiiAinimations {
                 // Each card will appear sequentially with the delay of 300ms
                 query(':enter', stagger('500ms', [
                 animate('.5s .3s ease-in', keyframes([
-                    style({ opacity: 0, transform: 'translateY(-50%)', offset: 0 }),
-                    style({ opacity: .5, transform: 'translateY(-10px) scale(1.1)', offset: 0.3 }),
-                    style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
+                    style({ opacity: 0, transform: 'translateX(-100%)', offset: 0 }),
+                    style({ opacity: .5, transform: 'translateX(-10px)', offset: 0.3 }),
+                    style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
                 ]))]), { optional: true }),
 
                 // Cards will disappear sequentially with the delay of 300ms
                 query(':leave', stagger('500ms', [
-                animate('.5s .3s ease-out', keyframes([
+                animate('500ms .3s ease-out', keyframes([
                     style({ opacity: 1, transform: 'scale(1.1)', offset: 0 }),
                     style({ opacity: .5, transform: 'scale(.5)', offset: 0.3 }),
                     style({ opacity: 0, transform: 'scale(0)', offset: 1 }),
@@ -58,14 +58,14 @@ export class KiiAinimations {
                 // Each card will appear sequentially with the delay of 300ms
                 query(':enter', stagger('500ms', [
                 animate('.5s .3s ease-in', keyframes([
-                    style({ opacity: 0, transform: 'translateY(-50%)', offset: 0 }),
-                    style({ opacity: .5, transform: 'translateY(-10px) scale(1.1)', offset: 0.3 }),
-                    style({ opacity: 1, transform: 'translateY(0)', offset: 1 }),
+                    style({ opacity: 0, transform: 'translateX(-100%)', offset: 0 }),
+                    style({ opacity: .5, transform: 'translateX(-10px)', offset: 0.3 }),
+                    style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
                 ]))]), { optional: true }),
 
                 // Cards will disappear sequentially with the delay of 300ms
                 query(':leave', stagger('500ms', [
-                animate('.5s .3s ease-out', keyframes([
+                animate('500ms .3s ease-out', keyframes([
                     style({ opacity: 1, transform: 'scale(1.1)', offset: 0 }),
                     style({ opacity: .5, transform: 'scale(.5)', offset: 0.3 }),
                     style({ opacity: 0, transform: 'scale(0)', offset: 1 }),
