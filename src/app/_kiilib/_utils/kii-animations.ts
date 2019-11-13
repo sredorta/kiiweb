41a0611b18146855717b23faf359109acada75e4
sredorta <sergi.redorta@hotmail.com>
2019-11-13 18:20:11 +0100

    Standarized pages

--- src/app/_kiilib/_utils/kii-animations.ts
+++ src/app/_kiilib/_utils/kii-animations.ts
@@ -17,29 +17,11 @@ export class KiiAinimations {
     //Animation for blog elements
     public static blog() {
         return [
-            // Trigger animation cards array
             trigger('blogAnimation', [
-            // Transition from any state to any state
-            transition('* => *', [
-                // Initially the all cards are not visible
-                query(':enter', style({ opacity: 0 }), { optional: true }),
-
-                // Each card will appear sequentially with the delay of 300ms
-                query(':enter', stagger('500ms', [
-                animate('.5s .3s ease-in', keyframes([
-                    style({ opacity: 0, transform: 'translateX(-100%)', offset: 0 }),
-                    style({ opacity: .5, transform: 'translateX(-10px)', offset: 0.3 }),
-                    style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
-                ]))]), { optional: true }),
-
-                // Cards will disappear sequentially with the delay of 300ms
-                query(':leave', stagger('500ms', [
-                animate('500ms .3s ease-out', keyframes([
-                    style({ opacity: 1, transform: 'scale(1.1)', offset: 0 }),
-                    style({ opacity: .5, transform: 'scale(.5)', offset: 0.3 }),
-                    style({ opacity: 0, transform: 'scale(0)', offset: 1 }),
-                ]))]), { optional: true })
-            ]),
+                transition('* <=> *', [ 
+                    query('.element', style({opacity:0.1,transform: 'translateX(-100vw)'}),{optional:true}),
+                    query('.element', stagger('.3s', [animate('.3s  ease-in-out')]),{optional:true})
+                ])
             ])
         ]
     };
@@ -48,58 +30,22 @@ export class KiiAinimations {
     //Animation for prices elements
     public static prices() {
         return [
-            // Trigger animation cards array
             trigger('pricesAnimation', [
-            // Transition from any state to any state
-            transition('* => *', [
-                // Initially the all cards are not visible
-                query(':enter', style({ opacity: 0 }), { optional: true }),
-
-                // Each card will appear sequentially with the delay of 300ms
-                query(':enter', stagger('500ms', [
-                animate('.5s .3s ease-in', keyframes([
-                    style({ opacity: 0, transform: 'translateX(-100%)', offset: 0 }),
-                    style({ opacity: .5, transform: 'translateX(-10px)', offset: 0.3 }),
-                    style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
-                ]))]), { optional: true }),
-
-                // Cards will disappear sequentially with the delay of 300ms
-                query(':leave', stagger('500ms', [
-                animate('500ms .3s ease-out', keyframes([
-                    style({ opacity: 1, transform: 'scale(1.1)', offset: 0 }),
-                    style({ opacity: .5, transform: 'scale(.5)', offset: 0.3 }),
-                    style({ opacity: 0, transform: 'scale(0)', offset: 1 }),
-                ]))]), { optional: true })
-            ]),
+                transition('* <=> *', [ 
+                    query('.element', style({opacity:0.1,transform: 'translateX(-100vw)'}),{optional:true}),
+                    query('.element', stagger('.3s', [animate('.3s  ease-in-out')]),{optional:true})
+                ])
             ])
         ]
     };
 
     public static realisations() {
         return [
-            // Trigger animation cards array
             trigger('realisationsAnimation', [
-            // Transition from any state to any state
-            transition('* => *', [
-                // Initially the all cards are not visible
-                query(':enter', style({ opacity: 0 }), { optional: true }),
-
-                // Each card will appear sequentially with the delay of 300ms
-                query(':enter', stagger('500ms', [
-                animate('.5s .3s ease-in', keyframes([
-                    style({ opacity: 0, transform: 'translateX(-100%)', offset: 0 }),
-                    style({ opacity: .5, transform: 'translateX(-10px)', offset: 0.3 }),
-                    style({ opacity: 1, transform: 'translateX(0)', offset: 1 }),
-                ]))]), { optional: true }),
-
-                // Cards will disappear sequentially with the delay of 300ms
-                query(':leave', stagger('500ms', [
-                animate('500ms .3s ease-out', keyframes([
-                    style({ opacity: 1, transform: 'scale(1.1)', offset: 0 }),
-                    style({ opacity: .5, transform: 'scale(.5)', offset: 0.3 }),
-                    style({ opacity: 0, transform: 'scale(0)', offset: 1 }),
-                ]))]), { optional: true })
-            ]),
+                transition('* <=> *', [ 
+                    query('.element', style({opacity:0.1,transform: 'translateX(-100vw)'}),{optional:true}),
+                    query('.element', stagger('.3s', [animate('.3s  ease-in-out')]),{optional:true})
+                ])
             ])
         ]
     };
