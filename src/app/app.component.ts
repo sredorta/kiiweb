ded8e8a0924fcda3692d39587e9d6425dd7924e8
sredorta <sergi.redorta@hotmail.com>
2019-12-16 17:31:32 +0100

    Completed conversion to dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,13 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { environment } from '../environments/environment';
-import { SwUpdate } from '@angular/service-worker';
-import { Router } from '@angular/router';
-import { KiiBaseAbstract } from './_kiilib/_abstracts/kii-base.abstract';
-import { LocalizeRouterService } from './_kiilib/_libraries/localize-router';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kiiweb';
+export class AppComponent{
+  title = 'kubiiks';
   constructor() { }
   ngOnInit() {
 
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  Inject, PLATFORM_ID } from '@angular/core';
+import { KiiBaseAuthAbstract } from '../../_kiilib/_abstracts/kii-base-auth.abstract';
+import { KiiApiAuthService } from '../../_kiilib/_services/kii-api-auth.service';
 
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss']
 })
-export class SidenavComponent implements OnInit {
+export class SidenavComponent extends KiiBaseAuthAbstract implements OnInit {
 
-  constructor() { }
+  constructor(private kiiApiAuth: KiiApiAuthService, @Inject(PLATFORM_ID) private platformId: any) {super(kiiApiAuth,platformId); }
 
   ngOnInit() {
+    this.getLoggedInUserSubscription();
   }
 
 }
